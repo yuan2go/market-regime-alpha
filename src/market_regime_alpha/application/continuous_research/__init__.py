82a29ffe198ca10f697c2ae1599c1c3d1a97d5d2
yuan.chen <yuan576264675@gmail.com>
2026-08-09 15:04:46 +0800

    fix(runtime): avoid continuous package import cycle

--- src/market_regime_alpha/application/continuous_research/__init__.py
+++ src/market_regime_alpha/application/continuous_research/__init__.py
@@ -22,11 +22,6 @@ from market_regime_alpha.application.continuous_research.change_detection import
 from market_regime_alpha.application.continuous_research.children import (
     ContinuousChildReference,
 )
-from market_regime_alpha.application.continuous_research.free_data_runtime import (
-    CanonicalFreeDataProvider,
-    CanonicalFreeDataResearchComposition,
-    ControlledRuntimeModelSelector,
-)
 from market_regime_alpha.application.continuous_research.scope import (
     ContinuousResearchScope,
     ContinuousResearchScopeRecord,
@@ -46,15 +41,12 @@ from market_regime_alpha.application.continuous_research.scheduler import (
 
 __all__ = [
     "ChangeDecision",
-    "CanonicalFreeDataProvider",
-    "CanonicalFreeDataResearchComposition",
     "ContinuousChildReference",
     "ContinuousResearchScope",
     "ContinuousResearchScopeRecord",
     "ContinuousResearchScheduleRunner",
     "ContinuousResearchTickRunner",
     "ContinuousTickExecutionResult",
-    "ControlledRuntimeModelSelector",
     "ContinuousScheduleRunResult",
     "ContinuousScheduleSnapshot",
     "ContinuousScheduleStatus",
